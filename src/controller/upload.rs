fbcfcf990f85fcb4d5f3eabc72c82519b90c8da4
yggverse <yggverse@project>
2025-12-28 18:44:34 +0200

    remove image compression, resizing, exif clear; return app errors instead of panic; remove breaking operation as the file was already saved on disk (in multipart mode) remove hardcoded image quantity limitation (use proxy settings instead, such as for size limit)

--- src/controller/upload.rs
+++ src/controller/upload.rs
@@ -19,14 +19,12 @@ use axum_extra::{
     headers::{Cookie, Referer},
 };
 use data_encoding::HEXLOWER;
-use image::{ImageFormat, imageops::FilterType};
-use img_parts::{DynImage, ImageEXIF};
-use mozjpeg::{ColorSpace, Compress, ScanMode};
+use image::ImageFormat;
 use ring::digest::{Context, SHA1_FOR_LEGACY_USE_ONLY};
 use serde::Deserialize;
 
 use tokio::fs::{self, remove_file};
-use tracing::error;
+use tracing::{error, warn};
 
 #[derive(Deserialize)]
 pub(crate) struct UploadPicParams {
@@ -234,99 +232,59 @@ pub(crate) async fn upload_post(
     let site_config = SiteConfig::get(&DB)?;
     let claim = Claim::get(&DB, &cookie, &site_config).ok_or(AppError::NonLogin)?;
 
-    let mut imgs = Vec::with_capacity(10);
+    let mut imgs = Vec::new();
     let mut batch = DB.inner().batch();
     let user_uploads = DB
         .inner()
         .open_partition("user_uploads", Default::default())?;
-    while let Some(field) = multipart.next_field().await.unwrap() {
-        if imgs.len() > 10 {
-            break;
-        }
-
+    while let Some(field) = multipart
+        .next_field()
+        .await
+        .map_err(|e| AppError::Custom(e.to_string()))?
+    {
         let data = match field.bytes().await {
             Ok(data) => data,
             Err(e) => {
-                error!("{:?}", e);
-                return Ok(e.into_response());
+                warn!("{:?}", e);
+                continue; // @TODO frontend alert
             }
         };
-
-        let image_format_detected = image::guess_format(&data)?;
-        let ext;
-        let img_data = match image_format_detected {
-            ImageFormat::Png | ImageFormat::Jpeg | ImageFormat::WebP => {
-                if let Ok(Some(mut img)) = DynImage::from_bytes(data) {
-                    img.set_exif(None);
-                    let img_noexif = img.encoder().bytes();
-
-                    // author: "Kim tae hyeon <kimth0734@gmail.com>"
-                    // https://github.com/altair823/image_compressor/blob/main/src/compressor.rs
-                    // license = "MIT"
-                    let dyn_img =
-                        image::load_from_memory_with_format(&img_noexif, image_format_detected)?;
-                    let factor = Factor::get(img_noexif.len());
-
-                    // resize
-                    let width = (dyn_img.width() as f32 * factor.size_ratio) as u32;
-                    let height = (dyn_img.width() as f32 * factor.size_ratio) as u32;
-                    let resized_img = dyn_img.resize(width, height, FilterType::Lanczos3);
-
-                    // compress
-                    let mut comp = Compress::new(ColorSpace::JCS_RGB);
-                    comp.set_scan_optimization_mode(ScanMode::Auto);
-                    comp.set_quality(factor.quality);
-
-                    let target_width = resized_img.width() as usize;
-                    let target_height = resized_img.height() as usize;
-                    comp.set_size(target_width, target_height);
-
-                    comp.set_optimize_scans(true);
-                    let mut comp = comp.start_compress(Vec::new()).unwrap();
-
-                    let mut line: usize = 0;
-                    let resized_img_data = resized_img.into_rgb8().into_vec();
-                    loop {
-                        if line > target_height - 1 {
-                            break;
-                        }
-                        let idx = line * target_width * 3..(line + 1) * target_width * 3;
-                        comp.write_scanlines(&resized_img_data[idx]).unwrap();
-                        line += 1;
-                    }
-
-                    if let Ok(comp) = comp.finish() {
-                        ext = "jpeg";
-                        comp
-                    } else {
-                        continue;
-                    }
-                } else {
-                    continue;
+        let format = match image::guess_format(&data) {
+            Ok(format) => format,
+            Err(e) => {
+                warn!("{:?}", e);
+                continue; // @TODO frontend alert
+            }
+        };
+        if !matches!(
+            format,
+            ImageFormat::Png | ImageFormat::Jpeg | ImageFormat::WebP | ImageFormat::Gif
+        ) {
+            warn!("Unsupported image format: {:?}", format);
+            continue;
+        } // @TODO frontend alert
+        match format.extensions_str().first() {
+            Some(extension) => {
+                let mut context = Context::new(&SHA1_FOR_LEGACY_USE_ONLY);
+                context.update(&data);
+                let filename = format!(
+                    "{}.{extension}",
+                    &HEXLOWER.encode(context.finish().as_ref()),
+                );
+                if let Err(e) =
+                    fs::write(format!("{}/{filename}", &CONFIG.upload_path), &data).await
+                {
+                    error!("{:?}", e);
+                    continue; // @TODO frontend alert
                 }
-            }
-            ImageFormat::Gif => {
-                ext = "gif";
-                data.to_vec()
-            }
-            _ => {
-                continue;
-            }
-        };
+                let img_id = incr_id(&DB, "imgs_count")?; // @TODO is this really work before the commit?
+                let k = [u32_to_ivec(claim.uid), u32_to_ivec(img_id)].concat();
+                batch.insert(&user_uploads, k, filename.as_bytes());
 
-        let mut context = Context::new(&SHA1_FOR_LEGACY_USE_ONLY);
-        context.update(&img_data);
-        let digest = context.finish();
-        let sha1 = HEXLOWER.encode(digest.as_ref());
-        let fname = format!("{}.{}", &sha1[0..20], ext);
-        let location = format!("{}/{}", &CONFIG.upload_path, fname);
-
-        fs::write(location, &img_data).await.unwrap();
-        let img_id = incr_id(&DB, "imgs_count")?;
-        let k = [u32_to_ivec(claim.uid), u32_to_ivec(img_id)].concat();
-        batch.insert(&user_uploads, k, fname.as_bytes());
-
-        imgs.push(fname);
+                imgs.push(filename)
+            }
+            None => warn!("Unsupported image extension"), // @TODO frontend alert
+        }
     }
 
     batch.commit()?;
@@ -341,49 +299,4 @@ pub(crate) async fn upload_post(
     };
 
     Ok(into_response(&page_upload))
-}
-
-#[derive(Copy, Clone)]
-struct Factor {
-    /// Quality of the new compressed image.
-    /// Values range from 0 to 100 in float.
-    quality: f32,
-
-    /// Ratio for resize the new compressed image.
-    /// Values range from 0 to 1 in float.
-    size_ratio: f32,
-}
-
-impl Factor {
-    /// Create a new `Factor` instance.
-    /// The `quality` range from 0 to 100 in float,
-    /// and `size_ratio` range from 0 to 1 in float.
-    ///
-    /// # Panics
-    ///
-    /// - If the quality value is 0 or less.
-    /// - If the quality value exceeds 100.
-    /// - If the size ratio value is 0 or less.
-    /// - If the size ratio value exceeds 1.
-    fn new(quality: f32, size_ratio: f32) -> Self {
-        if (quality > 0. && quality <= 100.) && (size_ratio > 0. && size_ratio <= 1.) {
-            Self {
-                quality,
-                size_ratio,
-            }
-        } else {
-            panic!("Wrong Factor argument!");
-        }
-    }
-
-    fn get(file_size: usize) -> Factor {
-        match file_size {
-            file_size if file_size > 5000000 => Factor::new(70., 0.75),
-            file_size if file_size > 1000000 => Factor::new(75., 0.8),
-            file_size if file_size > 600000 => Factor::new(80., 0.85),
-            file_size if file_size > 400000 => Factor::new(85., 0.9),
-            file_size if file_size > 200000 => Factor::new(90., 0.95),
-            _ => Factor::new(100., 1.0),
-        }
-    }
 }
